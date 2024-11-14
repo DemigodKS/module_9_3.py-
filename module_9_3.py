61dfa3130fbf5a3db6d13ca83db0e70f610a54de
DemigodKS <mankok222@gmail.com>
2024-11-14 18:09:01 +0300

    Update module_9_3.py

--- module_9_3.py
+++ module_9_3.py
@@ -4,7 +4,7 @@ second = ['Строка', 'Урбан', 'Компьютер']
 
 first_result = (len(x) - len(y) for x, y in zip(first, second) if len(x) != len(y))
 print(list(first_result))
-
+#1 вариант
 second_result = (len(first[i])==len(second[i]) for i in range(len(first)))
 print(list(second_result))
 #2 вариант
